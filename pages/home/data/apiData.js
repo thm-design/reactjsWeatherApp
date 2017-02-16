745d2e6fe04f60f0b1fbe035f420c599635883c3
Tony Meyer <thm-design@users.noreply.github.com>
2017-02-16 14:27:33 -0800

    Update apiData.js

--- pages/home/data/apiData.js
+++ pages/home/data/apiData.js
@@ -1,4 +1,4 @@
-const owmAPI = 'https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast/daily?id=6173331&cnt=7&units=metric&APPID=d6a8492b0d927353ebc1fb811d466922';
+const owmAPI = '#####api.openweathermap.org API CALL#####';
 const apiData = () => fetch(owmAPI)
   .then((response) => response.json())
   .then((result) => result)
